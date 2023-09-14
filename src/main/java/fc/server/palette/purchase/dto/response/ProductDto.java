0898e0e2d1745e24d91da175be9ae80ec9462be2
brotherstone97 <brotherstone97@gmail.com>
2023-09-14 19:35:51 +0900

    [#5] feat: 공동구매 리스트 응답 dto생성

---
+++ src/main/java/fc/server/palette/purchase/dto/response/ProductDto.java
@@ -0,0 +1,25 @@
+package fc.server.palette.purchase.dto.response;
+
+import fc.server.palette.purchase.entity.type.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String title;
+    private Category category;
+    private Date endDate;
+    private Time endTime;
+    private Integer price;
+    private String thumbnailUrl;
+    private Integer hits;
+}
