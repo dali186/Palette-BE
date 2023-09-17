f36fd08212452d2b39d77e9d426b9b6e9fc8a227
WooSeok77 <wi8tkf@naver.com>
2023-09-17 18:56:51 +0900

    [#6] feat: JwtAccessDeniedHandler, JwtAuthenticationEntryPoint 구현
    각각 필요한 권한없이 접근시 401, 403 Unauthorized 에러를 핸들링한다.

---
+++ src/main/java/fc/server/palette/member/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package fc.server.palette.member.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        //필요한 권한이 없이 접근하려 할때 403
+    }
+}
+
