bab3f99c3e2f90873d51e55b0bcde88e39fed0e8
WooSeok77 <wi8tkf@naver.com>
2023-09-17 18:46:50 +0900

    [#6] feat: TokenProvider 구현
    TokenProvider 구현및  application-dev.yml 파일에 jwt 설정추가

---
+++ src/main/java/fc/server/palette/member/jwt/TokenProvider.java
@@ -0,0 +1,96 @@
+package fc.server.palette.member.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class TokenProvider {
+
+    private static final String AUTHORITIES_KEY = "auth";
+
+    private final String secret;
+    private final long tokenValidityInMilliseconds;
+
+    private Key key;
+
+    public TokenProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds
+    ) {
+        this.secret = secret;
+        this.tokenValidityInMilliseconds = tokenValidityInSeconds;
+    }
+
+    @PostConstruct
+    public void setDecodedKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(Authentication authentication) {
+        log.info("authentication: {}",authentication);
+        String autorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, autorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        Collection<GrantedAuthority> authorities = AuthoritiesProvider.getAuthorityCollection();
+        User principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    //토큰 검증 수행
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.error("잘못된 JWT 서명입니다.");
+        }catch (ExpiredJwtException e){
+            log.error("만료된 JWT토큰입니다.");
+        }catch (UnsupportedJwtException e){
+            log.error("지원되지 않는 JWT토큰입니다.");
+        }catch (IllegalArgumentException e){
+            log.error("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+
+
+}
