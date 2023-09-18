9fc5b63316905f5edc641b051b6fbd28ea17c90a
brotherstone97 <brotherstone97@gmail.com>
2023-09-18 17:27:55 +0900

    [#5] feat: 공동구매 상세보기 컨트롤러 메서드 구현

--- src/main/java/fc/server/palette/purchase/controller/PurchaseController.java
+++ src/main/java/fc/server/palette/purchase/controller/PurchaseController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PurchaseController {
     private final PurchaseService purchaseService;
+
     @GetMapping("/groupPurchase")
-    public ResponseEntity<List<ProductDto>> getAllProducts(){
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
         List<ProductDto> products = purchaseService.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/groupPurchase/{productId}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long productId) {
+        ProductDto product = purchaseService.getProduct(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 }
