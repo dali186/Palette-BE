72c91b2ad234c5e86ddb1b4fbebbe276ffb5e40d
WooSeok77 <wi8tkf@naver.com>
2023-09-18 15:24:14 +0900

    [#6] feat: LoginDto, TokenDto 구현

---
+++ src/main/java/fc/server/palette/member/auth/dto/TokenDto.java
@@ -0,0 +1,12 @@
+package fc.server.palette.member.auth.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
