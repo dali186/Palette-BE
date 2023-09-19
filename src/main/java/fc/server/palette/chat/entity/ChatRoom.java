22f10b6864f339139cbb13dc91cb3d64e9d85703
JOOWON <wer07032@naver.com>
2023-09-19 17:12:17 +0900

    [FC-Palette#15] refactor: ChatRoom 어노테이션 수정
    @Builder 삭제 - 리스트 포함

--- src/main/java/fc/server/palette/chat/entity/ChatRoom.java
+++ src/main/java/fc/server/palette/chat/entity/ChatRoom.java
@@ -1,7 +1,6 @@
 package fc.server.palette.chat.entity;
 
 import fc.server.palette.chat.entity.type.ChatRoomType;
-import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,7 +10,6 @@ import java.util.List;
 
 @Data
 @Document(collection = "chatRoom")
-@Builder
 public class ChatRoom {
     @Id
     private String id;
