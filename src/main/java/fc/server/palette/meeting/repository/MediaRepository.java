bc7eb99c39d4c35b778cd0cfe0dc1bb0f56851c0
juhee <a07224@naver.com>
2023-09-20 16:32:32 +0900

    [#4] refactor: Repository 패키지 추가
    - body
      Repository 패키지 추가

--- src/main/java/fc/server/palette/meeting/repository/MediaRepository.java
+++ src/main/java/fc/server/palette/meeting/repository/MediaRepository.java
@@ -1,4 +1,4 @@
-package fc.server.palette.meeting.service;
+package fc.server.palette.meeting.repository;
 
 import fc.server.palette.meeting.entity.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
