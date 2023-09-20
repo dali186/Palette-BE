18f5d43172bf6efc6c4364ec076f29143547a147
brotherstone97 <brotherstone97@gmail.com>
2023-09-20 19:55:15 +0900

    [#5] feat: 공동구매 참여자와 member의 매핑 테이블 생성

---
+++ src/main/java/fc/server/palette/purchase/entity/ParticipantMember.java
@@ -0,0 +1,27 @@
+package fc.server.palette.purchase.entity;
+
+import fc.server.palette.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "group_purchase_participant_member")
+public class ParticipantMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    private PurchaseParticipant purchaseParticipant;
+}
