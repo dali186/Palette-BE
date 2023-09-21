95b782e7835a132beadc2629d080e9a707706464
JOOWON <wer07032@naver.com>
2023-09-21 21:55:03 +0900

    [FC-Palette#27] feat: 공지기능, 삭제 기능 메서드 구현

--- src/main/java/fc/server/palette/chat/controller/ChatController.java
+++ src/main/java/fc/server/palette/chat/controller/ChatController.java
@@ -1,8 +1,11 @@
 package fc.server.palette.chat.controller;
 
+import fc.server.palette.chat.dto.request.ChatRoomNoticeRequestDto;
 import fc.server.palette.chat.entity.ChatMessage;
-import fc.server.palette.chat.service.ChatService;
+import fc.server.palette.chat.entity.ChatRoom;
+import fc.server.palette.chat.entity.type.ChatRoomType;
 import fc.server.palette.chat.service.ChatRoomService;
+import fc.server.palette.chat.service.ChatService;
 import fc.server.palette.member.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +16,7 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
@@ -29,7 +29,7 @@ public class ChatController {
 
     //채팅방 목록 조회
     @GetMapping("/list")
-    public ResponseEntity<?> getChatRoomList(@RequestParam("type") String type, @AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<?> chatRoomList(@RequestParam("type") String type, @AuthenticationPrincipal CustomUserDetails userDetails) {
         Long memberId = userDetails.getMember().getId();
 
         if (type.equals("p")) {
@@ -38,6 +38,45 @@ public class ChatController {
 
         return ResponseEntity.ok(chatMessageService.setChatRoomListResponse(chatRoomService.findGroupChatRoomList(memberId), memberId));
     }
+
+    //공지 등록
+    @PostMapping("/notice")
+    public ResponseEntity<?> noticeSave(@RequestBody ChatRoomNoticeRequestDto request) {
+        ChatRoom chatRoom = chatRoomService.findChatRoomById(request.getRoomId());
+        chatRoom.getNoticeList().add(request.getMessageId());
+        chatRoomService.updateChatRoom(chatRoom);
+
+        return ResponseEntity.ok(request);
+    }
+
+    //공지 리스트 조회
+    //TODO memberimageUrl 추가 로직
+    @GetMapping("/notice")
+    public ResponseEntity<?> noticeList(@RequestParam("roomId") String roomId) {
+        ChatRoom chatRoom = chatRoomService.findChatRoomById(roomId);
+
+        return ResponseEntity.ok(chatMessageService.setChatRoomNoticeListResponse(chatRoom));
+    }
+
+    //채팅방 나가기
+    @DeleteMapping("/exit")
+    public ResponseEntity<?> memberRemove(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam("roomId") String roomId) {
+        ChatRoom chatRoom = chatRoomService.findChatRoomById(roomId);
+        chatRoom.getMemberList().remove(userDetails.getMember().getId());
+        chatRoom.getExitList().remove(userDetails.getMember().getId());
+
+        if (chatRoom.getType().equals(ChatRoomType.MEETING) || chatRoom.getType().equals(ChatRoomType.PURCHASE)) {
+            //TODO 채팅방에 나갔습니다 메세지 발송
+        }
+
+        chatRoomService.updateChatRoom(chatRoom);
+
+        return ResponseEntity.ok("삭제되었습니다");
+    }
+
+    /**
+     * related to Socket
+     */
 
     @MessageMapping("/chat/send")
     @SendTo("/sub/public/{roomId}")
--- src/main/java/fc/server/palette/chat/dto/response/ChatRoomListResponseDto.java
+++ src/main/java/fc/server/palette/chat/dto/response/ChatRoomListResponseDto.java
@@ -5,15 +5,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
+@Setter
 @Builder
 public class ChatRoomListResponseDto {
     private String id;
     private String title;
     private String image;
-    private List<Long> memberList;
+    private Integer members;
     private String recentMessage;
     private LocalDateTime recentTime;
     private Long unRead;
