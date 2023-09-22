2cc8ead97c836057057975d610fd5f2cb8d78205
brotherstone97 <brotherstone97@gmail.com>
2023-09-22 17:53:43 +0900

    [#26] fix: 저장된 썸네일이 없을 때 url값으로 null을 리턴하도록 수정
    추후 예외처리 로직 구현 시 예외를 던지도록 변경할 예정입니다.

--- src/main/java/fc/server/palette/secondhand/service/SecondhandService.java
+++ src/main/java/fc/server/palette/secondhand/service/SecondhandService.java
@@ -1,6 +1,7 @@
 package fc.server.palette.secondhand.service;
 
 import fc.server.palette.secondhand.dto.ProductListDto;
+import fc.server.palette.secondhand.entity.Media;
 import fc.server.palette.secondhand.entity.Secondhand;
 import fc.server.palette.secondhand.repository.SecondhandBookmarkRepository;
 import fc.server.palette.secondhand.repository.SecondhandMediaRepository;
@@ -20,14 +21,14 @@ public class SecondhandService {
     private final SecondhandBookmarkRepository secondhandBookmarkRepository;
 
     @Transactional(readOnly = true)
-    public List<ProductListDto> getAllProducts(){
+    public List<ProductListDto> getAllProducts() {
         List<Secondhand> products = secondhandRespository.findAll();
         return products.stream()
                 .map(this::buildProductList)
                 .collect(Collectors.toList());
     }
 
-    private ProductListDto buildProductList(Secondhand secondhand){
+    private ProductListDto buildProductList(Secondhand secondhand) {
         return ProductListDto.builder()
                 .id(secondhand.getId())
                 .title(secondhand.getTitle())
@@ -39,13 +40,18 @@ public class SecondhandService {
                 .build();
     }
 
-    private String getThumbnailUrl(Long secondhandId){
+    private String getThumbnailUrl(Long secondhandId) {
+        List<Media> mediaList = secondhandMediaRepository.findAllBySecondhand_id(secondhandId);
+        if (mediaList.isEmpty()) {
+            //todo 예외발생으로 변경
+            return null;
+        }
         return secondhandMediaRepository.findAllBySecondhand_id(secondhandId)
                 .get(0)
                 .getUrl();
     }
 
-    private Integer getBookmarkCount(Long secondhandId){
+    private Integer getBookmarkCount(Long secondhandId) {
         return secondhandBookmarkRepository.findAllBySecondhand_id(secondhandId).size();
     }
 }
