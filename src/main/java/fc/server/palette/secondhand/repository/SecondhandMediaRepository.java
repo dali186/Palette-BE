f8d2a0409870765c45fa664c52939fcc5e0e8b80
brotherstone97 <brotherstone97@gmail.com>
2023-09-22 14:40:09 +0900

    [#26] feat: 중고거래 id에 media를 모두 반환하는 repository메서드 선언

---
+++ src/main/java/fc/server/palette/secondhand/repository/SecondhandMediaRepository.java
@@ -0,0 +1,10 @@
+package fc.server.palette.secondhand.repository;
+
+import fc.server.palette.secondhand.entity.Media;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SecondhandMediaRepository extends JpaRepository<Media, Long> {
+    List<Media> findAllBySecondhand_id(Long secondhandId);
+}
