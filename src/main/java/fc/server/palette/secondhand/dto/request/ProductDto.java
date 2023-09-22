875e1c804c09db5954e558d475a4a6c10ab3dd9a
brotherstone97 <brotherstone97@gmail.com>
2023-09-22 21:16:34 +0900

    [#26] feat: 중고거래 등록 dto 생성

---
+++ src/main/java/fc/server/palette/secondhand/dto/request/ProductDto.java
@@ -0,0 +1,36 @@
+package fc.server.palette.secondhand.dto.request;
+
+import fc.server.palette.member.entity.Member;
+import fc.server.palette.purchase.entity.type.Category;
+import fc.server.palette.secondhand.entity.Secondhand;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.sql.Time;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ProductDto {
+    private String title;
+    private Category category;
+    private Time transactionStartTime;
+    private Time transactionEndTime;
+    //todo 요일 필드
+    private Boolean isFree;
+    private String description;
+    private Integer price;
+
+    public Secondhand toEntity(Member member){
+        return Secondhand.builder()
+                .member(member)
+                .title(this.title)
+                .category(this.category)
+                .transactionStartTime(this.transactionStartTime)
+                .transactionEndTime(this.transactionEndTime)
+                .price(this.price)
+                .description(this.description)
+                .build();
+    }
+}
