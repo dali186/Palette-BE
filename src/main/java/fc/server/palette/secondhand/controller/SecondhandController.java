5790fa1fa0d0fce717525f53d2b2963f790f5cfc
brotherstone97 <brotherstone97@gmail.com>
2023-09-22 19:41:53 +0900

    [#26] feat: 중고거래 상세보기 컨트롤러 메서드 구현

--- src/main/java/fc/server/palette/secondhand/controller/SecondhandController.java
+++ src/main/java/fc/server/palette/secondhand/controller/SecondhandController.java
@@ -1,11 +1,13 @@
 package fc.server.palette.secondhand.controller;
 
+import fc.server.palette.secondhand.dto.ProductDto;
 import fc.server.palette.secondhand.dto.ProductListDto;
 import fc.server.palette.secondhand.service.SecondhandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +16,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/secondhand")
 @RequiredArgsConstructor
-public class SecondhandController{
+public class SecondhandController {
     private final SecondhandService secondhandService;
 
     @GetMapping("")
-    public ResponseEntity<List<ProductListDto>> getAllProducts(){
+    public ResponseEntity<List<ProductListDto>> getAllProducts() {
         List<ProductListDto> products = secondhandService.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
+
+    @GetMapping("{productId}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long productId) {
+        ProductDto product = secondhandService.getProduct(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
 }
