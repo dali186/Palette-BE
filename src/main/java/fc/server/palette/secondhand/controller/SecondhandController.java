24de784b040ea0cc0c5402c468838d285881f552
brotherstone97 <brotherstone97@gmail.com>
2023-09-22 14:56:16 +0900

    [#26] feat: 중고거래 리스트 컨트롤러 메서드 구현

---
+++ src/main/java/fc/server/palette/secondhand/controller/SecondhandController.java
@@ -0,0 +1,25 @@
+package fc.server.palette.secondhand.controller;
+
+import fc.server.palette.secondhand.dto.ProductListDto;
+import fc.server.palette.secondhand.service.SecondhandService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/secondhand")
+@RequiredArgsConstructor
+public class SecondhandController{
+    private final SecondhandService secondhandService;
+
+    @GetMapping("")
+    public ResponseEntity<List<ProductListDto>> getAllProducts(){
+        List<ProductListDto> products = secondhandService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+}
