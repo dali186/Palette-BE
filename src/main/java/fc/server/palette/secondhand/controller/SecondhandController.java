2aeac32fc5f9e7db6cb31e0658fe8f253b740233
brotherstone97 <brotherstone97@gmail.com>
2023-09-24 03:00:20 +0900

    [#26] feat: 중고거래 등록 컨트롤러 구현

--- src/main/java/fc/server/palette/secondhand/controller/SecondhandController.java
+++ src/main/java/fc/server/palette/secondhand/controller/SecondhandController.java
@@ -1,14 +1,20 @@
 package fc.server.palette.secondhand.controller;
 
+import fc.server.palette.member.auth.CustomUserDetails;
+import fc.server.palette.secondhand.dto.request.CreateProductDto;
 import fc.server.palette.secondhand.dto.response.ProductDto;
 import fc.server.palette.secondhand.dto.response.ProductListDto;
+import fc.server.palette.secondhand.entity.Media;
+import fc.server.palette.secondhand.entity.Secondhand;
 import fc.server.palette.secondhand.service.SecondhandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/secondhand")
@@ -38,5 +44,22 @@ public class SecondhandController {
     public ResponseEntity<ProductDto> closeTransaction(@PathVariable Long productId) {
         ProductDto product = secondhandService.closeTransaction(productId);
         return new ResponseEntity<ProductDto>(product, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ProductDto> createProduct(@RequestBody CreateProductDto createProductDto,
+                                                    @AuthenticationPrincipal CustomUserDetails userDetails){
+        Secondhand product = createProductDto.toEntity(userDetails.getMember());
+        List<Media> mediaList = toMediaList(createProductDto.getImages(), product);
+        ProductDto savedProduct = secondhandService.createProduct(product, mediaList);
+        return new ResponseEntity<>(savedProduct, HttpStatus.OK);
+    }
+
+    private List<Media> toMediaList(List<String> images, Secondhand secondhand) {
+        return images.stream().map(url -> Media.builder()
+                        .secondhand(secondhand)
+                        .url(url)
+                        .build())
+                .collect(Collectors.toList());
     }
 }
