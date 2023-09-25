0779b4c623ad703b422ce30c7bfe0c847d64a908
brotherstone97 <brotherstone97@gmail.com>
2023-09-25 16:53:38 +0900

    [#26] fix: 중고거래, 공동구매 양방향 매핑 설정
    중고거래 글, 공동구매 글 삭제 시 자식엔티티인 미디어와 북마크엔티티도 함께 제거하기 위해 양방향 엔티티로 수정 + CascadeType=REMOVE 및 orphanRemoval=true로 수정함

--- src/main/java/fc/server/palette/secondhand/entity/Secondhand.java
+++ src/main/java/fc/server/palette/secondhand/entity/Secondhand.java
@@ -5,14 +5,15 @@ import fc.server.palette.member.entity.Member;
 import fc.server.palette.purchase.entity.type.Category;
 import fc.server.palette.secondhand.dto.request.EditProductDto;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 import java.sql.Time;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -59,10 +60,18 @@ public class Secondhand extends BaseEntity {
     @Column(nullable = false)
     private Boolean isFree;
 
+    @OneToMany(mappedBy = "secondhand", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Media> mediaList;
+
+    @OneToMany(mappedBy = "secondhand", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Bookmark> bookmarks;
+
+
     // db에 저장된 기본값이 적용되지 않고 필드에 null로 저장되는데 따른 조치
     public Boolean getIsSoldOut() {
         return isSoldOut != null && isSoldOut;
     }
+
     public Integer getHits() {
         return hits == null ? 0 : hits;
     }
@@ -75,7 +84,7 @@ public class Secondhand extends BaseEntity {
         this.isSoldOut = true;
     }
 
-    public void updateProduct(EditProductDto editProductDto){
+    public void updateProduct(EditProductDto editProductDto) {
         this.price = editProductDto.getPrice();
         this.transactionStartTime = editProductDto.getTransactionStartTime();
         this.transactionEndTime = editProductDto.getTransactionEndTime();
