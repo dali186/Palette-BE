74c4352ffb57f1aa8d1938922efe1ae78f921d88
brotherstone97 <brotherstone97@gmail.com>
2023-09-27 19:19:38 +0900

    [#33] feat: 디렉토리 이름 상수 클래스 생성

---
+++ src/main/java/fc/server/palette/_common/s3/S3DirectoryNames.java
@@ -0,0 +1,9 @@
+package fc.server.palette._common.s3;
+
+public class S3DirectoryNames {
+    public static final String CHAT = "chat";
+    public static final String MEETING = "meeting";
+    public static final String PURCHASE = "purchase";
+    public static final String SECONDHAND = "secondhand";
+    public static final String MEMBER = "member";
+}
