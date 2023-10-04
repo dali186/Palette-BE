1c6a24c9fd095f86bdc3660559c737f04b9e7a63
brotherstone97 <brotherstone97@gmail.com>
2023-10-05 02:53:31 +0900

    [#56] feat: 현재 상세보기를 제외한 특정 멤버의 모든 게시물을 반환하는 repository메서드 구현
    조건이 복잡해 쿼리가 필요한 관계로 JPQL사용했습니다.

--- src/main/java/fc/server/palette/secondhand/repository/SecondhandRespository.java
+++ src/main/java/fc/server/palette/secondhand/repository/SecondhandRespository.java
@@ -2,6 +2,12 @@ package fc.server.palette.secondhand.repository;
 
 import fc.server.palette.secondhand.entity.Secondhand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SecondhandRespository extends JpaRepository<Secondhand, Long> {
+
+    @Query("select s from Secondhand s where s.member.id = ?1 and s.id <> ?2")
+    List<Secondhand> findAllByMemberIdAndExcludeId(Long memberId, Long idToExclude);
 }
